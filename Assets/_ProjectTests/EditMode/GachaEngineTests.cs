5db55b169ab2e482ff82c67ae08895411a8d4d25
iloat20 <iloat20@github.com>
2026-07-28 16:20:21 +0800

    fix(domain): correct HardPity test (5 calls to reach threshold, distinguishable weights)

--- Assets/_ProjectTests/EditMode/GachaEngineTests.cs
+++ Assets/_ProjectTests/EditMode/GachaEngineTests.cs
@@ -31,10 +31,17 @@ public class GachaEngineTests
     [Test]
     public void HardPity_Triggers_At_Threshold()
     {
+        // Natural roll is always R; pity forces SSR (rarity 3) at the threshold.
         var pity = new PityCounter(5);
-        Rarity r1 = pity.RollWithPity(new System.Random(1), new int[] { 0, 0, 100, 0 }, 5);
-        Rarity r5 = pity.RollWithPity(new System.Random(5), new int[] { 0, 0, 100, 0 }, 5);
-        Assert.AreNotEqual(Rarity.SSR, r1);
-        Assert.AreEqual(Rarity.SSR, r5);
+        Rarity r1 = pity.RollWithPity(new System.Random(1), new int[] { 100, 0, 0, 0 }, 3);
+        Rarity r2 = pity.RollWithPity(new System.Random(2), new int[] { 100, 0, 0, 0 }, 3);
+        Rarity r3 = pity.RollWithPity(new System.Random(3), new int[] { 100, 0, 0, 0 }, 3);
+        Rarity r4 = pity.RollWithPity(new System.Random(4), new int[] { 100, 0, 0, 0 }, 3);
+        Rarity r5 = pity.RollWithPity(new System.Random(5), new int[] { 100, 0, 0, 0 }, 3);
+        Assert.AreEqual(Rarity.R, r1);
+        Assert.AreEqual(Rarity.R, r2);
+        Assert.AreEqual(Rarity.R, r3);
+        Assert.AreEqual(Rarity.R, r4);
+        Assert.AreEqual(Rarity.SSR, r5);   // forced at threshold
     }
 }
